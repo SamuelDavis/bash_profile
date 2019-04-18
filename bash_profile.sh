c91c84589936567b9626df6d70503acfc25615ea
Samuel Davis <samueldavis@gmail.com>
2019-04-18 19:46:50 -0400

    support optional .bash_private parsing for private overrides

---
+++ bash_profile.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# PROMPT HELPERS
+function ref(){
+    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/" || return
+}
+
+function sha(){
+    git rev-parse --short HEAD 2> /dev/null | sed -e "s/\(.*\)/\[\1\]/" || return
+}
+
+function dateOut() {
+    date +%H:%M || return
+}
+
+# STYLE
+RED="\[\e[0;31m\]"
+YELLOW="\[\e[0;33m\]"
+GREEN="\[\e[0;32m\]"
+WHITE="\[\e[0;37m\]"
+
+PS1="$GREEN[\$(dateOut)]$YELLOW[\$(ref)]\$(sha)\[\e[0m\][\w]\$ "
+
+if [ -f ./.bash_private ]; then
+    source ./.bash_private
+fi
+
